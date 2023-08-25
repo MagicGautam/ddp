762e508308f8312fd93d5ece0d71056adfb9d7e0
Gautam <gupta.gautam889@gmail.com>
2023-08-26 01:01:57 +0530

    changes to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/posts', // Replace with your API route path
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+      // Add more routes if needed
+    ];
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
